93e43a11e6cf9bec26fbd36ed20f31760ee1c9ac
wnsdnn <a24369029@gmail.com>
2023-11-24 15:51:04 +0900

    init

---
+++ learn-vue3/.eslintrc.cjs
@@ -0,0 +1,19 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution')
+
+module.exports = {
+  root: true,
+  'extends': [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-prettier/skip-formatting'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  "rules": {
+    // "no-console": "error",
+    "no-console": process.env.NODE_ENV === "production" ? "error" :"warn",
+    "prettier/prettier": [],
+  }
+}
---
+++ learn-vue3/src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+</script>
+
+<template>
+  <h2>Hello Vue3</h2>
+</template>
+
+<style scoped>
+
+</style>
