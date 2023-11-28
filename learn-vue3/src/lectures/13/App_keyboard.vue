4189cb3ca01e8e6ef369d0ef0e8aff63943aa5e6
wnsdnn <a24369029@gmail.com>
2023-11-28 17:26:40 +0900

    이벤트 처리

---
+++ learn-vue3/src/lectures/13/App_keyboard.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <input type="text" @keyup.ctrl.enter="addTodo">
+        <ul>
+            <li v-for="(todo, index) in todos" :key="index">
+                {{ todo }}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import { reactive } from 'vue';
+
+export default {
+    setup () {
+        const todos = reactive([]);
+        const addTodo = (event) => {
+            todos.push(event.target.value);
+            event.target.value = '';
+            event.target.focus();
+            // if(event.key === 'Enter') {
+            //     console.log('event.key: ', event.key);
+            //     todos.push(event.target.value);
+            // }
+        }
+
+        return {
+            todos,
+            addTodo
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ learn-vue3/src/lectures/13/App_mouse.vue
@@ -0,0 +1,77 @@
+<template>
+    <div>
+        <div id="modifiers">
+            <div @click="clickDiv">
+                DIV 영역
+                <p @click.self="clickP">
+                    P 영역
+                    <!-- .stop === e.stopPropagation()  -->
+                    <!-- <span @click.stop="clickSpan"> SPAN 영역 </span> -->
+                    <!-- 연달아서 써서 다 실행할수 있다 -->
+                    
+                    <span @click="clickSpan"> SPAN 영역 </span>
+                    
+                    <a href="https://naver.com" @click.prevent.stop="clickA">a 영역</a>
+
+                </p>
+            </div>
+        </div>
+        <button @click.once="clickDiv">once</button>
+        <!-- .prevent === e.preventDefault() -->
+        <!-- <a href="https://naver.com" @click.prevent="clickA">a 영역</a> -->
+    </div>
+</template>
+
+<script>
+export default {
+    setup () {
+        const clickDiv = () => {
+            console.log('clickDiv');
+            // location.href = 'https://naver.com';
+        };
+
+        const clickP = () => {
+            console.log('clickP');
+        };
+
+        const clickSpan = () => {
+            console.log('clickSpan');
+            // alert('좋아요');
+        };
+
+        const clickA = () => {
+            alert('어떤기능~');
+        };
+
+
+
+        return {
+            clickDiv,
+            clickP,
+            clickSpan,
+            clickA,
+        }
+    }
+}
+</script>
+
+<style scoped>
+    #modifiers div, 
+    #modifiers p, 
+    #modifiers span {
+        display: block;
+        padding: 40px;
+    }
+
+    #modifiers div {
+        background-color: #ccc;
+    }
+
+    #modifiers p {
+        background-color: #999;
+    }
+
+    #modifiers span {
+        background-color: #666;
+    }
+</style>
