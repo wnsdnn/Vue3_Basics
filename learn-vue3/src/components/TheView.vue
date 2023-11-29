bb15f2797be177593f0a8a85912230221c24ff98
wnsdnn <a24369029@gmail.com>
2023-11-29 16:04:37 +0900

    컴포넌트 기초

---
+++ learn-vue3/src/components/TheView.vue
@@ -0,0 +1,46 @@
+<template>
+    <main>
+        <div class="container text-center py-4">
+            <div class="row g-5">
+                <div class="col col-4">
+                    <!-- 카멜 케이스도 가능 -->
+                    <app-card></app-card>
+                </div>
+                <div class="col col-4">
+                    <AppCard></AppCard>
+                </div>
+                <div class="col col-4">
+                    <AppCard></AppCard>
+                </div>
+                <div class="col col-4">
+                    <AppCard></AppCard>
+                </div>
+                <div class="col col-4">
+                    <AppCard></AppCard>
+                </div>
+                <div class="col col-4">
+                    <AppCard></AppCard>
+                </div>
+            </div>
+        </div>
+    </main>
+</template>
+
+<script>
+import AppCard from './AppCard.vue';
+
+export default {
+    components: {
+        AppCard
+    },
+    setup () {
+        
+
+        return {}
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- learn-vue3/src/main.js
+++ learn-vue3/src/main.js
@@ -3,7 +3,13 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { createApp } from 'vue'
 import App from './App.vue'
+// import AppCard from '@/components/AppCard.vue';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+// 컴포넌트 전역 등록
+// app.component('AppCard', AppCard);
+
+app.mount('#app');
 
 import 'bootstrap/dist/js/bootstrap.js';
