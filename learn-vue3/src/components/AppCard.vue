f9cd32139ee6d1793528cdc8fd05b24bb042d917
wnsdnn <a24369029@gmail.com>
2023-11-30 13:15:40 +0900

    props

--- learn-vue3/src/components/AppCard.vue
+++ learn-vue3/src/components/AppCard.vue
@@ -1,31 +1,88 @@
 <template>
     <div class="card">
         <div class="card-body">
-            <h5 class="card-title red">Card title</h5>
+            <!-- {{ $props }} -->
+            <!-- type: news, notice -->
+            <span class="badge bg-secondary">{{ typeName }}</span>
+            <h5 class="card-title red mt-2">{{ title }}</h5>
             <!-- :class="$style.red" -->
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+            <p class="card-text">{{ contents }}</p>
+            <a href="#" class="btn" :class="isLikeClass" @click="toggleLike">좋아요</a>
+            {{ $props.obj }}
         </div>
     </div>
 </template>
 
 <script>
-import { ref, useCssModule } from 'vue';
+import { computed, ref, useCssModule } from 'vue';
 
-console.log('AppCard module');
 
 export default {
-    setup () {
-        console.log('AppCard setup()');
-
+    props: {
+        type: {
+            type: String,
+            default: 'news',
+            // 유효성검사
+            validator: (value) => {
+                return ['news', 'notice'].includes(value);
+            },
+        },
+        title: {
+            type: String,
+            required: true,
+        },
+        contents: {
+            type: String,
+            required: true,
+        },
+        isLike: {
+            type: Boolean,
+            default: false,
+        },
+        obj: {
+            type: Object,
+            // 기본값을 함수로 이렇게 실행해야함
+            default: function() {
+                return {};
+            }
+        }
+    },
+    emits: ['toggleLike'],
+    setup (props, context) {
         // const style = useCssModule();
         // console.log('style: ', style);
 
         const color = ref('blue');
         color.value = 'green';
 
+
+        // ====================================================
+
+        console.log('props.title: ', props.title);
+
+        const isLikeClass = computed(() => {
+            return props.isLike ? 'btn-danger' : 'btn-outline-danger';
+        });
+
+        const typeName = computed(() => {
+            return props.type === 'news' ? '뉴스' : '공지사항';
+        });
+
+
+        const toggleLike = () => {
+            // 하위컴포넌트에서는 변경 못함
+            // props.isLike = !props.isLike;
+
+            // 이벤트 발생시키기
+            context.emit('toggleLike');
+        };
+
+
         return {
             color,
+            isLikeClass, 
+            typeName, 
+            toggleLike,
         }
     }
 }
--- learn-vue3/src/components/TheView.vue
+++ learn-vue3/src/components/TheView.vue
@@ -2,24 +2,17 @@
     <main>
         <div class="container text-center py-4">
             <div class="row g-5">
-                <div class="col col-4">
-                    <!-- 카멜 케이스도 가능 -->
-                    <app-card></app-card>
-                </div>
-                <div class="col col-4">
-                    <AppCard></AppCard>
-                </div>
-                <div class="col col-4">
-                    <AppCard></AppCard>
-                </div>
-                <div class="col col-4">
-                    <AppCard></AppCard>
-                </div>
-                <div class="col col-4">
-                    <AppCard></AppCard>
-                </div>
-                <div class="col col-4">
-                    <AppCard></AppCard>
+                <div v-for="post in posts" :key="post.id" class="col col-4">
+                    <AppCard 
+                        :title="post.title" 
+                        :contents="post.contents" 
+                        :type="post.type"
+                        :is-like="post.isLike"
+                        @toggle-like="post.isLike = !post.isLike"
+                        :obj="obj"
+                    />
+
+                    <!-- <button @click="post.isLike = !post.isLike">toggle</button> -->
                 </div>
             </div>
         </div>
@@ -27,6 +20,7 @@
 </template>
 
 <script>
+import { reactive } from 'vue';
 import AppCard from './AppCard.vue';
 
 export default {
@@ -34,9 +28,23 @@ export default {
         AppCard
     },
     setup () {
-        
+        const obj = reactive({
+            title: '제목2',
+            contents: '내용2',
+        });
 
-        return {}
+        const posts = reactive([
+            { id: 1, title: '제목1', contents: '내용1', isLike: true, type: 'news' },
+            { id: 2, title: '제목2', contents: '내용2', isLike: true, type: 'news' },
+            { id: 3, title: '제목3', contents: '내용3', isLike: true, type: 'news' },
+            { id: 4, title: '제목4', contents: '내용4', isLike: false, type: 'notice' },
+            { id: 5, title: '제목5', contents: '내용5', isLike: false, type: 'notice' },
+        ]);
+
+        return {
+            obj,
+            posts,
+        }
     }
 }
 </script>
